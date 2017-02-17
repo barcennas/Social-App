3727c518d75170bc677dcd451f247fce58c2649c
Abraham Barcenas M <abrahamz96@hotmail.com>
2017-02-16 18:13:30 -0600

    Creation of Prroject

---
+++ Devslopes Social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Devslopes Social
+//
+//  Created by Abraham Barcenas M on 2/16/17.
+//  Copyright © 2017 barcennas. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
