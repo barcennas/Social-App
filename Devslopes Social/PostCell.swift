615f2ec03a3cb1ad638b26cd498419e9be436b64
Abraham Barcenas M <abrahamz96@hotmail.com>
2017-02-19 23:19:27 -0600

    Added table view custom cell in feed

---
+++ Devslopes Social/PostCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostCell.swift
+//  Devslopes Social
+//
+//  Created by Abraham Barcenas M on 2/19/17.
+//  Copyright © 2017 barcennas. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImage : UIImageView!
+    @IBOutlet weak var userNameLbl : UILabel!
+    @IBOutlet weak var postImage : UIImageView!
+    @IBOutlet weak var caption : UITextView!
+    @IBOutlet weak var numberLikesLbl : UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
